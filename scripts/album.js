e6ead06b01f7fdf56206f51dedcc9f5af4feae02
oceansource <oceansource.gmail.com>
2016-09-05 07:15:41 -0700

    Add click feature to album cover - assignment 25

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,20 @@ var albumMarconi = {
   ]
 };
 
+var albumMine = {
+  title: 'Hello',
+  artist: 'Ty Hatton',
+  label: 'TH',
+  year: '2017',
+  albumArtUrl: 'assets/images/album_covers/21.png',
+  songs: [
+    { title: 'Hello this is Ty', duration: '1:01' },
+    { title: 'Who\'s there?', duration: '5:01' },
+    { title: 'Must be a crank call', duration: '3:21'},
+    { title: 'I\'m blocking your number', duration: '3:14' },
+  ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
   var template =
       '<tr class="album-view-song-item">'
@@ -40,20 +54,24 @@ var createSongRow = function(songNumber, songName, songLength) {
   return template;
  };
 
-var setCurrentAlbum = function(album) {
-  var albumTitle = document.getElementsByClassName('album-view-title')[0];
-  var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-  var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-  var albumImage = document.getElementsByClassName('album-cover-art')[0];
-  var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+//Select elements that we want to populate with text dynamically
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
+var setCurrentAlbum = function(album) {
+  //Assign values to each part of the album (text, images)
   albumTitle.firstChild.nodeValue = album.title;
   albumArtist.firstChild.nodeValue = album.artist;
   albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
   albumImage.setAttribute('src', album.albumArtUrl);
 
+  //Clear content of album list container
   albumSongList.innerHTML = '';
 
+  //Build list of songs from album JS object
   for (var i = 0; i < album.songs.length; i++) {
     albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
   }
@@ -61,4 +79,14 @@ var setCurrentAlbum = function(album) {
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+   
+   var albums = [albumPicasso, albumMarconi, albumMine];
+   var index = 1;   
+   albumImage.addEventListener("click", function(event) {
+     setCurrentAlbum(albums[index]);
+     index++;
+     if (index == albums.length) {
+       index = 0;
+     }
+   });
  };
